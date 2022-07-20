a5b2bc566c0ee14d493c67d4a42e811026e601bb
Xavier Baldacchino <xbaldacchino@gmail.com>
2022-07-20 12:54:05 +0200

    validate modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -13,8 +13,8 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const closeBtn = document.querySelectorAll(".close");
 const formData = document.querySelectorAll(".formData");
 const form = document.getElementById("form");
-console.log(form);
-
+const closeBtnValidate = document.querySelectorAll(".close-validate");
+const modalbg2 = document.querySelector(".bground2");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -27,26 +27,131 @@ function launchModal() {
 // close modal event
 closeBtn.forEach((close) => close.addEventListener("click", closeModal));
 
+// close validate modal event
+closeBtnValidate.forEach((close) =>
+  close.addEventListener("click", closeModalValidate)
+);
+
 // close modal form
 function closeModal() {
   modalbg.style.display = "none";
 }
 
-// when user submits form
-form.addEventListener('submit', function(e){
-  e.preventDefault();
-const firstName = document.getElementById('first').value;
-
-let firstNameIsValid = false;
-
-if (firstName.length < 3) {
-  console.log('erreurPrénom');
-} else {
-  firstNameIsValid = true;
+// close validate modal form
+function closeModalValidate() {
+  modalbg2.style.display = "none";
 }
 
-if (firstNameIsValid) {
-  console.log('Afficher la modal de validation')
-} 
+function validateEmail(email) {
+  return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+}
 
-})
+// when user submits form
+
+//firstname
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const firstName = document.getElementById("first").value;
+
+  let firstNameIsValid = false;
+
+  if (firstName.length < 3) {
+    document.querySelector(".first-name-form").style.display = "block";
+  } else {
+    firstNameIsValid = true;
+    document.querySelector(".first-name-form").style.display = "none";
+  }
+
+  //lastname
+  const lastName = document.getElementById("last").value;
+
+  let lastNameIsValid = false;
+
+  if (lastName.length < 3) {
+    document.querySelector(".last-name-form").style.display = "block";
+  } else {
+    lastNameIsValid = true;
+    document.querySelector(".last-name-form").style.display = "none";
+  }
+
+  //email
+  const email = document.getElementById("email").value;
+
+  let emailIsValid = false;
+
+  if (!validateEmail(email)) {
+    document.querySelector(".email-form").style.display = "block";
+  } else {
+    emailIsValid = true;
+    document.querySelector(".email-form").style.display = "none";
+  }
+
+  //birthdate
+  const birthdate = document.getElementById("birthdate").value;
+  let birthdateIsValid = false;
+  var birthdate_regex = /\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])*/;
+
+  if (birthdate_regex.test(birthdate)) {
+    birthdateIsValid = true;
+    document.querySelector(".birthdate-form").style.display = "none";
+  } else {
+    document.querySelector(".birthdate-form").style.display = "block";
+  }
+
+  //championship quantity
+  const championshipQuantity = parseInt(
+    document.getElementById("quantity").value
+  );
+  let championshipQuantityIsValid = false;
+
+  if (!isNaN(championshipQuantity)) {
+    championshipQuantityIsValid = true;
+    document.querySelector(".quantity-form").style.display = "none";
+  } else {
+    document.querySelector(".quantity-form").style.display = "block";
+  }
+
+  //location
+  const locations = document.querySelectorAll(".checkbox-loc");
+
+  let locationsIsValid = false;
+  locations.forEach((location) => {
+    if (location.checked) {
+      locationsIsValid = true;
+    }
+  });
+
+  if (!locationsIsValid) {
+    document.querySelector(".location-form").style.display = "block";
+  } else {
+    document.querySelector(".location-form").style.display = "none";
+  }
+
+  //checkbox
+  const checkboxTerms = document.querySelector(".checkbox-terms");
+  let checkboxTermsIsValid = false;
+  if (checkboxTerms.checked) {
+    checkboxTermsIsValid = true;
+    document.querySelector(".terms-form").style.display = "none";
+  } else {
+    document.querySelector(".terms-form").style.display = "block";
+  }
+
+  //modal
+  if (
+    firstNameIsValid &&
+    lastNameIsValid &&
+    emailIsValid &&
+    birthdateIsValid &&
+    championshipQuantityIsValid &&
+    locationsIsValid &&
+    checkboxTermsIsValid
+  ) {
+    closeModal();
+    modalbg2.style.display = "block";
+  }
+});
